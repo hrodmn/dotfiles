040b81cef96b7ef3ad08ecea211fee4303364f27
hrodmn <henry.rodman@gmail.com>
2020-05-01 07:12:49 -0500

    point R installation to bionic repository

--- scripts/programs/R.sh
+++ scripts/programs/R.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
-sudo apt install r-base -y
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran40/'
+sudo apt install -y r-base r-cran-spatial
