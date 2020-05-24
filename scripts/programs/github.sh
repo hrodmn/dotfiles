b42c8d746fe8869a4aecf9aa2479ba274751c0bd
hrodmn <henry.rodman@gmail.com>
2020-05-24 06:26:17 -0500

    add script to clone ST repos

---
+++ scripts/programs/github.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd ~/workspace \
+  && git clone git@github.com:SilviaTerra/aux-stats.git \
+  && git clone git@github.com:SilviaTerra/landsat_blender.git \
+  && git clone git@github.com:SilviaTerra/landsatviewer.git \
+  && git clone git@github.com:SilviaTerra/sequoia.git \
+  && git clone git@github.com:SilviaTerra/st_aws.git \
+  && git clone git@github.com:SilviaTerra/st_basemap.git \
+  && git clone git@github.com:SilviaTerra/st_fia_utils.git \
+  && git clone git@github.com:SilviaTerra/st_fiadb.git \
+  && git clone git@github.com:SilviaTerra/st_growth.git \
+  && git clone git@github.com:SilviaTerra/st_height.git \
+  && git clone git@github.com:SilviaTerra/st_plotdata.git \
+  && git clone git@github.com:SilviaTerra/st_volume.git \
+  && git clone git@github.com:SilviaTerra/stcore.git \
+  && git clone git@github.com:SilviaTerra/tidyFIA.git \
+  && git clone git@github.com:SilviaTerra/ts_algo.git \
+  && git clone git@github.com:SilviaTerra/ts_utils.git
