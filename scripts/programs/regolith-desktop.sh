8264ddeaf08763c1079fac9775c4ebdda13f861d
hrodmn <henry.rodman@gmail.com>
2020-05-24 06:26:40 -0500

    add additional regolith system monitoring widgets

--- scripts/programs/regolith-desktop.sh
+++ scripts/programs/regolith-desktop.sh
@@ -1,4 +1,12 @@
 # regolith-desktop
 sudo add-apt-repository -y ppa:regolith-linux/release
 
-sudo apt install -y regolith-desktop i3xrocks-net-traffic i3xrocks-cpu-usage i3xrocks-time i3xrocks-battery
+sudo apt install -y  \
+  regolith-desktop \
+  i3xrocks-net-traffic \
+  i3xrocks-cpu-usage \
+  i3xrocks-time \
+  i3xrocks-battery \
+  i3xrocks-volume \
+  i3xrocks-temp \
+  i3xrocks-weather
