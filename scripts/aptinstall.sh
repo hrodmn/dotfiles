251c2f26f45718d632df38c944db0afdf161a93f
hrodmn <henry.rodman@gmail.com>
2020-05-24 06:25:09 -0500

    don't use custom install function

--- scripts/aptinstall.sh
+++ scripts/aptinstall.sh
@@ -2,21 +2,17 @@
 
 sudo apt update
 
-function install {
-  which $1 &> /dev/null
-
-  if [ $? -ne 0 ]; then
-    echo "Installing: ${1}..."
-    sudo apt install -y $1
-  else
-    echo "Already installed: ${1}"
-  fi
-}
-
-# Basics
-install awscli
-install curl
-install git
-install htop
-install vim
-
+sudo apt install -y \
+  awscli \
+  build-essential \
+  curl \
+  giada \
+  git \
+  libglpk-dev \
+  htop \
+  libcairo2-dev \
+  libffi-dev \
+  libfontconfig1-dev \
+  libssl-dev \
+  pandoc \
+  xclip
