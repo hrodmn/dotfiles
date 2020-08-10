d713f015dba64e358720a17e73ccdf5c546ee2f6
hrodmn <henry.rodman@gmail.com>
2020-08-10 05:44:12 -0500

    update Rstudio

--- scripts/programs/Rstudio.sh
+++ scripts/programs/Rstudio.sh
@@ -2,6 +2,6 @@
 sudo apt update
 gpg --keyserver keys.gnupg.net --recv-keys 3F32EE77E331692F
 sudo apt install gdebi-core
-wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.2.5042-amd64.deb
-sudo gdebi rstudio-1.2.5042-amd64.deb
-rm rstudio-1.2.5042-amd64.deb
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.3.959-amd64.deb
+sudo gdebi rstudio-1.3.959-amd64.deb
+rm rstudio-1.3.959-amd64.deb
